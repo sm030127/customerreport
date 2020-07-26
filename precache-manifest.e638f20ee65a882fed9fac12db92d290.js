4dce945038839a96c3fabb097493c034ed803e63
Suarabh Mishra <saurmishra@deloitte.com>
2020-07-27 00:36:01 +0530

    Updates

---
+++ precache-manifest.e638f20ee65a882fed9fac12db92d290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0b2a09f4c3c46619c1b2c539e09f15",
+    "url": "/customerreport/index.html"
+  },
+  {
+    "revision": "a6121517a2df24029c9c",
+    "url": "/customerreport/static/css/main.6accfeee.chunk.css"
+  },
+  {
+    "revision": "28173fe75cc00a462134",
+    "url": "/customerreport/static/js/2.f68384fe.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/customerreport/static/js/2.f68384fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6121517a2df24029c9c",
+    "url": "/customerreport/static/js/main.7ece6451.chunk.js"
+  },
+  {
+    "revision": "c3b0ec414fb7c927c3be",
+    "url": "/customerreport/static/js/runtime-main.8a850bc4.js"
+  }
+]);
